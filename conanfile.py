5a22a5477437cd82d0410ff3451ea7b464dfe620
Patrick Wang-Freninger <github@freninger.at>
2020-07-19 14:11:01 +0200

    Update to Catch 2.13.0

--- conanfile.py
+++ conanfile.py
@@ -2,12 +2,12 @@ from conans import ConanFile, CMake, tools
 
 class PrebuiltCatch2(ConanFile):
     name = "prebuilt-catch2"
-    version = "2.7.1"
+    version = "2.13.0"
     url = "https://github.com/catchorg/Catch2"
-    license = "MIT"
+    license = "BSL-1.0"
     description = "Repackaging of Catch2 with a prebuilt main.cpp."
     settings = "os", "compiler", "arch", "build_type"
-    requires = "Catch2/2.7.1@catchorg/stable"
+    requires = "Catch2/2.13.0@catchorg/stable"
     exports_sources = "**"
     generators = "cmake"
     build_policy = "missing"
